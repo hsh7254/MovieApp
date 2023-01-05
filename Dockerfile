4e2ec64ae3ae39eb82c7e96d838bf7ef06fe428f
tjs0330 <htjsgh0330@gmail.com>
2023-01-05 20:55:28 +0900

    reset

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
+# server image는 node.js 사용
 FROM node:18-alpine
 ENV NODE_ENV=production
 WORKDIR /app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
+# 3000port 사용
 EXPOSE 3000
 RUN chown -R node /usr/src/app
 USER node
--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food( { name, picture, rating }){
-  return (
-    <div>
-      <h2>I like { name }</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={picture} alt={name}/>
-    </div>
-  )
+class App extends React.Component{
+  state = {
+    count: 0,
+  };
+
+  render(){
+    return <h1>The number is : {this.state.count}</h1>;
+  }
 }
-
-const foodILike = [
-  {
-    id: 1,
-    name: 'kimchi',
-    image: 'http://www.realkimchi.com/shopimages/realkimchi/001001000001.jpg?1669457620',
-    rating: 5,
-  },
-  {
-    id: 2,
-    name: 'Samgiopsal',
-    image: 'https://src.hidoc.co.kr/image/lib/2021/8/27/1630049987719_0.jpg',
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: 'Bibimbap',
-    image: 'https://health.chosun.com/site/data/img_dir/2021/01/27/2021012702508_0.jpg',
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: 'Doncasu',
-    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2021/10/28/4e06df24f7acc52f449251b387f644981.jpg',
-    rating: 4.7,
-  },
-  {
-    id: 5,
-    name: 'Kimbap',
-    image: 'https://recipe1.ezmember.co.kr/cache/recipe/2016/06/29/e7401296033ab8e4297cd53d71e1bba91.jpg',
-    rating: 4.6,
-  },
-];
-
-function App() {
-  return(
-    <div>
-      {foodILike.map(dish =>(
-        <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating} />
-      ))}
-    </div>
-  )
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  picture: PropTypes.string.isRequired,
-  rating: PropTypes.number,
-};
 
 export default App;
