dc8d97d67c000d77fc908f60d0419549834e613f
tjs0330 <htjsgh0330@gmail.com>
2023-01-10 22:40:00 +0900

    css update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-import './App.css';
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -41,6 +41,7 @@ class App extends React.Component {
                 title={movie.title}
                 summary={movie.summary}
                 poster={movie.medium_cover_image}
+                genres={movie.genres}
               />
             ))}
           </div>
